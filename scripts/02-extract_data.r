a6e3ba8d81df5cdf9fc0f8d7ca0c298e3378791f
Gibran Hemani <explodecomputer@gmail.com>
2017-05-24 19:42:30 +0100

    updated

--- scripts/02-extract_data.r
+++ scripts/02-extract_data.r
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(TwoSampleMR)
 
-toggle_dev("test")
+#toggle_dev("test")
 
 try_extract_outcome <- function(SNP, id, tries=3)
 {
@@ -19,19 +19,21 @@ try_extract_outcome <- function(SNP, id, tries=3)
 
 ###
 
-ds <- commandArgs(T)[1]
+ds <- "01"
 
 load(paste0("../results/", ds, "/outcome_nodes.rdata"))
 load(paste0("../results/", ds, "/exposure_dat.rdata"))
 
+snps <- unique(exposure_dat$SNP)
+
 m <- list()
 for(i in 1:nrow(outcome_nodes))
 {
-	temp <- try_extract_outcome(exposure_dat$SNP, outcome_nodes$id[i])
+	temp <- try_extract_outcome(snps, outcome_nodes$id[i])
 	m[[i]] <- filter(temp, mr_keep.outcome) %>%
 		select(id.outcome, SNP, effect_allele.outcome, other_allele.outcome, eaf.outcome, beta.outcome, se.outcome, pval.outcome, samplesize.outcome, ncase.outcome, ncontrol.outcome, proxy.outcome, proxy_snp.outcome)
 }
 
 outcome_datl <- m
-save(outcome_datl, file=paste0("../data/results/", ds, "/outcome_extract.rdata"))
+save(outcome_datl, file=paste0("../data/results/", ds, "/outcome_extract_", sec, ".rdata"))
 
